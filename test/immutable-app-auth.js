e66245e00cb7825a3bd3809c1c062bc5923ebb61
Ersun Warncke <ersun.warncke@gmail.com>
2017-01-29 13:10:27 -0400

    add database to test

--- test/immutable-app-auth.js
+++ test/immutable-app-auth.js
@@ -5,24 +5,58 @@ const immutableApp = require('immutable-app')
 // load auth module
 const auth = require('../lib/immutable-app-auth')
 
+const ImmutableDatabaseMariaSQL = require('immutable-database-mariasql')
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 const httpClient = require('immutable-http-client')
+const immutable = require('immutable-core')
 
 chai.use(chaiAsPromised)
 const assert = chai.assert
+
+const dbHost = process.env.DB_HOST || 'localhost'
+const dbName = process.env.DB_NAME || 'test'
+const dbPass = process.env.DB_PASS || ''
+const dbUser = process.env.DB_USER || 'root'
+
+// use the same params for all connections
+const connectionParams = {
+    charset: 'utf8',
+    db: dbName,
+    host: dbHost,
+    password: dbPass,
+    user: dbUser,
+}
 
 describe('immutable-app-auth', function () {
 
     var app
 
+    // create database connection to use for testing
+    var database = new ImmutableDatabaseMariaSQL(connectionParams)
+
     beforeEach(async function () {
         // catch async errors
         try {
+            // reset immutable modules
+            immutable.reset()
+            // drop any test tables
+            await database.query('DROP TABLE IF EXISTS account')
+            await database.query('DROP TABLE IF EXISTS auth')
+            await database.query('DROP TABLE IF EXISTS device')
+            await database.query('DROP TABLE IF EXISTS deviceSession')
+            await database.query('DROP TABLE IF EXISTS session')
+            await database.query('DROP TABLE IF EXISTS sessionAccount')
+            // reset global app config
+            await immutableApp.reset()
             // create new app instance 
             app = immutableApp('test-app')
             // set configuration for testing
             app.config({
+                // set default database
+                database: {
+                    default: database,
+                },
                 // do not exit on listen errors
                 exit: false,
                 // do not log
